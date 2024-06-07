b52f776676c7a62bd3cd7c661ef09ee7a9d1cc19
sahver <29501754+sahver@users.noreply.github.com>
2024-06-07 11:37:33 +0300

    Added update.sh to update code without installing dependencies nor overwriting configuration

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Error out if anything fails.
+set -e
+
+# Make sure script is run as root.
+if [ "$(id -u)" != "0" ]; then
+  echo "Must be run as root with sudo! Try: sudo ./install.sh"
+  exit 1
+fi
+
+echo "Updating video_looper program..."
+echo "=================================="
+
+python3 setup.py install --force
+
+echo "Finished!"
