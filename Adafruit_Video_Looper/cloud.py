23beec40f58f4f985e1cf36d786542eb9e9fea1d
sahver <29501754+sahver@users.noreply.github.com>
2024-12-12 14:29:07 +0200

    Haapsalu speed fix

--- Adafruit_Video_Looper/cloud.py
+++ Adafruit_Video_Looper/cloud.py
@@ -180,7 +180,7 @@ class CloudGrid:
         self._print(f'_calc_diff(): y={self._crop_y} len={self._video_length} speed={self._video_speed}')
         self._print(f'_calc_diff(): ({self._video_length} - ({self._video_length} * {self._crop_y})) / {self._video_speed} = {(self._video_length - (self._video_length * self._crop_y)) / self._video_speed}')
 #        return (self._video_length - (self._video_length * self._crop_y)) / self._video_speed
-        return (self._video_length - (self._video_length * self._crop_y)) / self._video_speed - 26.208332843250698 # HAAPSALU
+        return (self._video_length - (self._video_length * self._crop_y)) / self._video_speed - 33.69642794132233 # HAAPSALU, SPEED=28
     def _router_listen(self):
         # If already connected, shutdown first
         if self._router:
